0727ff530ec42e1e0fea1207da866d931227634a
Ari Becker <ari-becker@users.noreply.github.com>
2020-07-16 09:50:18 +0300

    Rename repo in copy/paste

--- .githooks/check-format.sh
+++ .githooks/check-format.sh
@@ -2,9 +2,9 @@
 
 set -euo pipefail
 
-echo '[INFO][dhall-types-svc][.githooks/check-format.sh] Checking formatting...'
+echo '[INFO][dhall-concourse][.githooks/check-format.sh] Checking formatting...'
 while IFS= read -r -d '' dhall_file ; do
-  echo "[INFO][dhall-types-svc][.githooks/check-format.sh] Checking: ${dhall_file} ..."
+  echo "[INFO][dhall-concourse][.githooks/check-format.sh] Checking: ${dhall_file} ..."
   dhall format --check --inplace "${dhall_file}"
 done < <(find . -name '*.dhall' -not -path './legacy/*' -print0)
-echo '[INFO][dhall-types-svc][.githooks/check-format.sh] Finished checking formatting successfully!'
+echo '[INFO][dhall-concourse][.githooks/check-format.sh] Finished checking formatting successfully!'
